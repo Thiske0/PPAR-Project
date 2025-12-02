b86ebf7e9a427b876454789f5284845593b97235
Mathis Poppe <mathispoppe1@gmail.com>
2025-12-02 22:27:15 +0100

    moved .vscode/c_cpp_properties.json

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash 
 
-#OAR -l host=2,walltime=0:10:00
-#OAR -O mandel_OAR_%jobid%.out
-#OAR -E mandel_OAR_%jobid%.err
+#OAR -l host=2/core=18,walltime=0:10:00
+#OAR -O mitm_OAR_%jobid%.out
+#OAR -E mitm_OAR_%jobid%.err
+
+cat $OAR_NODEFILE
 
 # We compile and run on the compute nodes because of the -march=native flag
 mpicc -O3 -march=native -Wall -o mitm_mpi_grouped mitm_mpi_grouped.c -fopenmp
-mpiexec -ppn 1 --hostfile $OAR_NODEFILE ./mitm_mpi_grouped --n 20 --online
+mpiexec  --map-by ppr:1:node --hostfile $OAR_NODEFILE ./mitm_mpi_grouped --n 20 --online
