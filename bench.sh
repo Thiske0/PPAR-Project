0b0d36d4966efdead046d76867653babd2bd7cc5
Mathis Poppe <mathispoppe1@gmail.com>
2025-12-03 21:17:10 +0100

    fixed script

--- bench.sh
+++ bench.sh
@@ -38,20 +38,3 @@ for fill in "${GROUPS_COUNT_FILL[@]}"; do
         done
     done
 done
-
-
-
-gcc -Wall -o make_header make_header.c
-
-./make_header --fill-groups $GROUPS_COUNT_FILL --probe-groups $GROUPS_COUNT_PROBE --buffer-size $BUFFER_SIZE --bsend-amount $BSEND_AMOUNT
-
-# We compile and run on the compute nodes because of the -march=native flag
-mpicc -O3 -march=native -Wall -o mitm_mpi_grouped mitm_mpi_grouped.c -fopenmp
-
-mpiexec \
-    --mca pml ob1 \
-    --mca btl ^openib \
-    --bind-to none \
-    --map-by ppr:1:node \
-    --hostfile $OAR_NODEFILE \
-    ./mitm_mpi_grouped --n $N --online
